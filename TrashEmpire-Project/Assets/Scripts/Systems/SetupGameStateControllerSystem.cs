e237ff319081e64ff0221737e4785439e06fbd42
Johnny <jthompson@tmg.dev>
2021-11-27 21:19:13 -0800

    Started creating a gameplay level.

---
+++ TrashEmpire-Project/Assets/Scripts/Systems/SetupGameStateControllerSystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+
+namespace TMG.TrashEmpire
+{
+    //[UpdateInGroup(typeof(BeginInitializationEntityCommandBufferSystem), OrderFirst = true)]
+    public class SetupGameStateControllerSystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+}
